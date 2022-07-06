5d8c98e2f115c7b0c02be8160168e7d4cb5b3007
mattpettenato <mattpettenato@gmail.com>
2022-07-06 16:43:14 -0700

    user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,31 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.balance = params[:balance]
+
+    if @user.balance >= 0
+      @user.save!
+      render :show
+    else
+      render json: ['Unable to process request']
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
